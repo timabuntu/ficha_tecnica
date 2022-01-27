6075fe13233bb85d8be753255a7f5aeb6328a13c
thiago lopes de mello <timabuntu@gmail.com>
2022-01-27 10:27:09 -0300

    feat: created data product

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
 
 const App: React.FC = () =>  {
+  const product = [
+  {
+    item: 'abobrinha',
+    amountPaid: 2.5,
+    quantity: 1000,
+    valuePerItem: 2.5
+  },
+  {
+    item: 'cebola',
+    amountPaid: 3.7,
+    quantity: 1000,
+    valuePerItem: 3.7
+  },
+  {
+    item: 'chuchu',
+    amountPaid: 1.2,
+    quantity: 1000,
+    valuePerItem: 1.2
+  },
+  {
+    item: 'morango',
+    amountPaid: 5,
+    quantity: 1000,
+    valuePerItem: 5
+  },
+]
+
   return (
     <div>
       <h1>Ficha Técnica</h1>
