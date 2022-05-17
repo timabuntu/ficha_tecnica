bb1f323c54a183214f6a3f39518d98f54c7b2feb
thiago lopes de mello <timabuntu@gmail.com>
2022-05-17 09:56:03 -0300

    chore: reseted project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import ProductList from "./components/ProductList";
-
-const App: React.FC = () =>  {
+const App: React.FC = () => {
   return (
     <div>
       <h1>Ficha Técnica</h1>
-      <ProductList />
     </div>
   );
-}
+};
 
 export default App;
